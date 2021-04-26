e5c8856d414e3cf2b131230c205a24103760b852
AntonLovkin <anton28111987@gmail.com>
2021-04-27 00:27:25 +0300

    27.04
    поиск работает
    грузится картинка со значками
    работает Window.scrollTo()

---
+++ src/api.js
@@ -0,0 +1,54 @@
+export default class NewApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+    
+    fetchArticles() {
+                    
+        const API_KEY = '21271693-9f15050c6f3761d48e024dcb2';
+        const BASE_URL = 'https://pixabay.com/';
+      
+    return fetch(`${BASE_URL}api/?image_type=photo&orientation=horizontal&q=cat&page=${this.page}&per_page=12&key=${API_KEY}`)
+            .then(r => r.json())
+            .then(data => {
+                console.log(data);
+                this.page += 1;
+                
+                return data.hits;
+            });
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+
+}
+
+
+// Тебе интересны следующие свойства:
+
+// webformatURL - ссылка на маленькое изображение для списка карточек
+// largeImageURL - ссылка на большое изображение (смотри пункт 'дополнительно')
+// likes - количество лайков
+// views - количество просмотров
+// comments - количество комментариев
+// downloads - количество загрузок
+
+// const options = {
+//             headers: {
+//                 authorization: '21271693-9f15050c6f3761d48e024dcb2'
+//             },
+//         };
+//         // const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12`
+//         fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=cat`, options)
+//             .then(r => r.json())
+//             .then(console.log);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import './styles.css';
+import NewApiService from './api';
+import hitsTpl from './hits.hbs';
+
+const refs = {
+    searchForm: document.querySelector('.js-search-form'),
+    galleryContainer: document.querySelector('.js-gallery'),
+    loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+}
+
+const newApiService = new NewApiService();
+console.log(newApiService);
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(el) {
+    el.preventDefault();
+    
+    newApiService.query = el.currentTarget.elements.query.value;
+    
+    newApiService.resetPage();
+    newApiService.fetchArticles().then(appendHitsMarcup);
+        
+}
+    function onLoadMore() {
+        newApiService.fetchArticles().then(appendHitsMarcup);
+        window.scrollTo({
+    top: 1200,
+    behavior: "behavior"
+});
+};
+
+function appendHitsMarcup(hits) {
+    refs.galleryContainer.insertAdjacentHTML('beforeend', hitsTpl(hits))
+}
+
